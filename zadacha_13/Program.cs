e1c98fe8407aeca5d526d87deedf1380962da026
Ilia_Z <akinoro19@gmail.com>
2022-07-13 14:14:40 +0300

    добавил математическое решение задачи 13

--- zadacha_13/Program.cs
+++ zadacha_13/Program.cs
@@ -2,8 +2,12 @@
 // Напишите программу, которая выводит третью цифру заданного 
 // числа или сообщает, что третьей цифры нет
 
+// Математический вариант решения
 Console.Write("Введите число :  ");
-string num = Console.ReadLine();
-int i = num.Length;
-if (i > 2)  Console.WriteLine(num[2]);
-else Console.WriteLine("третьей цифры нет");
+int num = int.Parse(Console.ReadLine());
+for (int i = 1; (num > 1000); i *= 10)
+{
+    num = num1 / i;
+}
+if (num > 99) Console.WriteLine($"Третья цифра  :  {num % 10}");
+else Console.WriteLine("Нет третьей цифры");
--- zadacha_15/Program.cs
+++ zadacha_15/Program.cs
@@ -2,8 +2,9 @@
 // Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
 // и проверяет, является ли этот день выходным.
 
-Console.WriteLine("Введите число ");
+Console.Write("Введите число : ");
 int num = int.Parse(Console.ReadLine());
-if ((num > 0) & (num < 6)) Console.WriteLine("Будний день, однако ");
-else if ((num > 5) & (num < 8)) Console.WriteLine("Ура! сегодня выходной!");
+string[] day = {"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
+if ((num > 0) & (num < 6)) Console.WriteLine($"{day[num-1]} - будний день, однако. ");
+else if ((num > 5) & (num < 8)) Console.WriteLine($"{day[num-1]} - Ура! сегодня выходной!");
 else Console.WriteLine("некоректный ввод");
